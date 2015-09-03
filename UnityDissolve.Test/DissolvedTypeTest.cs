7bb669786355d14a3115b9d0019c5f22e1c805ef
Igor Karatayev <igor.cwer@gmail.com>
2015-09-03 21:08:39 +0300

    added more unit tests.

--- UnityDissolve.Test/DissolvedTypeTest.cs
+++ UnityDissolve.Test/DissolvedTypeTest.cs
@@ -16,20 +16,125 @@ namespace UnityDissolve.Test
 				);
 
 		[Test]
-		public void AttributedClass()
+		public void TaggedClass()
 		{
-			var dt = new DissolvedType(typeof(SimpleDissolveClass));
+			var dt = new DissolvedType(typeof(TaggedDissolveClass));
 
 			Assert.That(dt.ComponentFields, Has.Count.EqualTo(3));
 			Assert.That(dt.AddComponentFields, Has.Count.EqualTo(0));
 			Assert.That(dt.SubComponents, Has.Count.EqualTo(0));
 
-			Assert.That(dt.ComponentFields, Has.Exactly(1).EqualTo(Tuple.Create<string, FieldInfo>("gameObject", null)).Using(DissolvedTypeItemComparer));
-			Assert.That(dt.ComponentFields, Has.Exactly(1).EqualTo(Tuple.Create<string, FieldInfo>("meshFilter", null)).Using(DissolvedTypeItemComparer));
-			Assert.That(dt.ComponentFields, Has.Exactly(1).EqualTo(Tuple.Create<string, FieldInfo>("meshCollider", null)).Using(DissolvedTypeItemComparer));
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "gameObject"));
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshFilter"));
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshCollider"));
 
-			Assert.That(dt.ComponentFields, Has.None.EqualTo(Tuple.Create<string, FieldInfo>("ommitedFieldString", null)).Using(DissolvedTypeItemComparer));
-			Assert.That(dt.ComponentFields, Has.None.EqualTo(Tuple.Create<string, FieldInfo>("ommitedFieldInt", null)).Using(DissolvedTypeItemComparer));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldString"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldInt"));
+		}
+
+		[Test]
+		public void TaggedClassWithAddComponentAndNamedField()
+		{
+			var dt = new DissolvedType(typeof(TaggedDissolveClassWithAddComponentsAndNamedField));
+
+			Assert.That(dt.ComponentFields, Has.Count.EqualTo(1));
+			Assert.That(dt.AddComponentFields, Has.Count.EqualTo(2));
+			Assert.That(dt.SubComponents, Has.Count.EqualTo(0));
+
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "gameObject"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshFilter"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshCollider"));
+
+			Assert.That(dt.AddComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "gameObject"));
+			Assert.That(dt.AddComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshFilter"));
+			Assert.That(dt.AddComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshCollider"));
+
+			Assert.That(dt.AddComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item1 == "Collider"));
+
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldString"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldInt"));
+		}
+
+		[Test]
+		public void TaggedClassWithNamedField()
+		{
+			var dt = new DissolvedType(typeof(TaggedDissolveClassWithNamedField));
+
+			Assert.That(dt.ComponentFields, Has.Count.EqualTo(3));
+			Assert.That(dt.AddComponentFields, Has.Count.EqualTo(0));
+			Assert.That(dt.SubComponents, Has.Count.EqualTo(0));
+
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "gameObject"));
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshFilter"));
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshCollider"));
+
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item1 == "Collider"));
+
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldString"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldInt"));
+		}
+
+		[Test]
+		public void UntaggedClassWithNamedField()
+		{
+			var dt = new DissolvedType(typeof(UntaggedDissolveClassWithNamedField));
+
+			Assert.That(dt.ComponentFields, Has.Count.EqualTo(2));
+			Assert.That(dt.AddComponentFields, Has.Count.EqualTo(0));
+			Assert.That(dt.SubComponents, Has.Count.EqualTo(0));
+
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "gameObject"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshFilter"));
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshCollider"));
+
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item1 == "Collider"));
+
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldString"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldInt"));
+		}
+
+		[Test]
+		public void UntaggedClassWithAddComponentAndNamedField()
+		{
+			var dt = new DissolvedType(typeof(UntaggedDissolveClassWithAddComponentsAndNamedField));
+
+			Assert.That(dt.ComponentFields, Has.Count.EqualTo(1));
+			Assert.That(dt.AddComponentFields, Has.Count.EqualTo(1));
+			Assert.That(dt.SubComponents, Has.Count.EqualTo(0));
+
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "gameObject"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshFilter"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshCollider"));
+
+			Assert.That(dt.AddComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "gameObject"));
+			Assert.That(dt.AddComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshFilter"));
+			Assert.That(dt.AddComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshCollider"));
+
+			Assert.That(dt.AddComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item1 == "Collider"));
+
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldString"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldInt"));
+		}
+
+		[Test]
+		public void UntaggedClassWithNestedComponents()
+		{
+			var dt = new DissolvedType(typeof(UntaggedDissolveClassWithNestedComponents));
+
+			Assert.That(dt.ComponentFields, Has.Count.EqualTo(1));
+			Assert.That(dt.AddComponentFields, Has.Count.EqualTo(0));
+			Assert.That(dt.SubComponents, Has.Count.EqualTo(1));
+
+			Assert.That(dt.ComponentFields, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "gameObject"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshFilter"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "meshCollider"));
+
+			Assert.That(dt.SubComponents, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "nestedClass"));
+
+			Assert.That(dt.SubComponents, Has.Exactly(1).Matches((Tuple<string, FieldInfo> i) => i.Item1 == "Nested"));
+
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldString"));
+			Assert.That(dt.ComponentFields, Has.None.Matches((Tuple<string, FieldInfo> i) => i.Item2.Name == "ommitedFieldInt"));
 		}
 	}
 }
--- UnityDissolve.Test/TestClasses.cs
+++ UnityDissolve.Test/TestClasses.cs
@@ -3,11 +3,68 @@
 namespace UnityDissolve.Test
 {
 	[Component]
-	public class SimpleDissolveClass
+	public class TaggedDissolveClass
 	{
 		public GameObject gameObject;
 		public MeshFilter meshFilter;
 		private MeshCollider meshCollider;
+
+		public string ommitedFieldString;
+		private int ommitedFieldInt;
+	}
+
+	[Component]
+	public class TaggedDissolveClassWithAddComponentsAndNamedField
+	{
+		public GameObject gameObject;
+		[AddComponent]
+		public MeshFilter meshFilter;
+		[AddComponent("Collider")]
+		private MeshCollider meshCollider;
+
+		public string ommitedFieldString;
+		private int ommitedFieldInt;
+	}
+
+	[Component]
+	public class TaggedDissolveClassWithNamedField
+	{
+		public GameObject gameObject;
+		public MeshFilter meshFilter;
+		[Component("Collider")] private MeshCollider meshCollider;
+	}
+
+	public class UntaggedDissolveClassWithNamedField
+	{
+		[Component]
+		public GameObject gameObject;
+		public MeshFilter meshFilter;
+		[Component("Collider")]
+		private MeshCollider meshCollider;
+
+		public string ommitedFieldString;
+		private int ommitedFieldInt;
+	}
+
+	public class UntaggedDissolveClassWithAddComponentsAndNamedField
+	{
+		[Component]
+		public GameObject gameObject;
+		public MeshFilter meshFilter;
+		[AddComponent("Collider")]
+		private MeshCollider meshCollider;
+
+		public string ommitedFieldString;
+		private int ommitedFieldInt;
+	}
+
+	public class UntaggedDissolveClassWithNestedComponents
+	{
+		[Component]
+		public GameObject gameObject;
+		public MeshFilter meshFilter;
+		[Component("Nested")]
+		private TaggedDissolveClass nestedClass;
 
 		public string ommitedFieldString;
 		private int ommitedFieldInt;
