c3edbfb7d603a5c6e565d6e8e1c7666241e133b0
Igor Karatayev <igor.cwer@gmail.com>
2017-09-10 17:53:48 +0800

    movde component setup function form UnityEngineEx.

--- src.editor/DissolvedObjectEditor.cs
+++ src.editor/DissolvedObjectEditor.cs
@@ -6,21 +6,51 @@ namespace UnityDissolve
 {
 	public class DissolvedObjectEditor
 	{
-		[MenuItem("Unity Dissolve/Dissolve All Objects on a Scene" )]
-		private static void DissolveAllObjects()
+		[MenuItem("GameObject/Dissolve", false, -100)]
+		static void Dissolve()
 		{
-			foreach (var component in Resources.FindObjectsOfTypeAll<Component>()) {
-				if (component.GetType().HasAttribute<DissolveInEditorAttribute>()) {
+			foreach (var component in Selection.activeGameObject.GetComponents<Component>())
+			{
+				if (component.GetType().HasAttribute<DissolveInEditorAttribute>())
+				{
 					component.Dissolve();
 				}
 			}
 		}
 
-		[MenuItem( "Unity Dissolve/Dissolve selected Object" )]
+		[MenuItem("GameObject/Dissolve", true)]
+		static bool ValidateDissolve()
+		{
+			foreach (var component in Selection.activeGameObject.GetComponents<Component>())
+			{
+				if (component.GetType().HasAttribute<DissolveInEditorAttribute>())
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		[MenuItem("Dissolve/Dissolve All Objects on the Scene")]
+		private static void DissolveAllObjects()
+		{
+			foreach (var component in Resources.FindObjectsOfTypeAll<Component>())
+			{
+				if (component.GetType().HasAttribute<DissolveInEditorAttribute>())
+				{
+					component.Dissolve();
+				}
+			}
+		}
+
+		[MenuItem("Dissolve/Dissolve Selected Object")]
 		private static void DissolveSelectedObject()
 		{
-			foreach (var component in Selection.activeGameObject.GetComponents<Component>()) {
-				if (component.GetType().HasAttribute<DissolveInEditorAttribute>()) {
+			foreach (var component in Selection.activeGameObject.GetComponents<Component>())
+			{
+				if (component.GetType().HasAttribute<DissolveInEditorAttribute>())
+				{
 					component.Dissolve();
 				}
 			}
---
+++ src/SetupEx.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using SystemEx;
+using UnityEngine;
+
+namespace UnityDissolve
+{
+	public static class SetupEx
+	{
+		/// <summary>
+		/// Sets SerializeFields of the Component to values form parameters object.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="c"></param>
+		/// <param name="parameters"></param>
+		/// <returns></returns>
+		public static T Setup<T>(this T c, object parameters) where T : Component
+		{
+			var fields = new Dictionary<string, FieldInfo>();
+			foreach (var field in c.GetType().GetFields<SerializeField>())
+			{
+				fields.Add(field.Name, field);
+			}
+			foreach (var property in parameters.GetType().GetProperties())
+			{
+				if (fields.ContainsKey(property.Name))
+				{
+					var field = fields[property.Name];
+					field.SetValue(c, property.GetValue(parameters, null));
+				}
+				else
+				{
+					Debug.LogWarning(String.Format("Property [{0}] not found int type [{1}]", property.Name, typeof(T).Name));
+				}
+			}
+
+			return c;
+		}
+
+		/// <summary>
+		/// Sets SerializeFields of the Component to values form parameters dictionary.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="c"></param>
+		/// <param name="parameters"></param>
+		/// <returns></returns>
+		public static T Setup<T>(this T c, IDictionary<string, object> parameters) where T : Component
+		{
+			foreach (var field in c.GetType().GetFields<SerializeField>())
+			{
+				object value;
+				if (parameters.TryGetValue(field.Name, out value))
+				{
+					field.SetValue(c, value);
+				}
+			}
+
+			return c;
+		}
+	}
+}
+
