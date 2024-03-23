33ca217c71eaf117a862c50f4964a7613720da4e
Igor Karatayev <igor.cwer@gmail.com>
2024-03-23 19:47:06 +0300

    update.

--- src/Attributes/DissolveInEditorAttribute.cs
+++ src/Attributes/DissolveInEditorAttribute.cs
@@ -2,6 +2,10 @@
 
 namespace UnityDissolve
 {
+	/// <summary>
+	/// Mark object as dissolvable for Unity menu -> Dissolve All objects on Scene
+	/// All objects marked with this attributre or with ExecuteInEditor attribute will be dissolved.
+	/// </summary>
 	public class DissolveInEditorAttribute : Attribute
 	{
 	}
